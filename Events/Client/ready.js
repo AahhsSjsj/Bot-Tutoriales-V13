cf060e31eb1b1e846c651240755a4f7ea61119dd
unknown_bom <73638811+AahhsSjsj@users.noreply.github.com>
2021-09-26 22:16:36 -0500

    Discord Bot V13

--- Events/Client/ready.js
+++ Events/Client/ready.js
@@ -1,9 +1,10 @@
 module.exports = {
     name: "ready",
-    execute(client) {
+    run: async(client) => {
         console.log(`${client.user.tag} Esta Listo!`)
         client.user.setActivity('Tu Texto PD: Mensaje Para Poner en el Estado!', {type: 'PLAYING' }) // PLAYING, WATCHING, LISTENING, STREAMING Nota: Para El STREAMING Debes Poner Asi:
         //type: "STREAMING",
-        //url: "twitch canal!" uwu! 
+        //url: "twitch canal"
+        //Osea client.user.setActivity('en Vivo', {type: 'STREAMING', url: 'https://www.twitch.tv/rubius' })
     }
 }
