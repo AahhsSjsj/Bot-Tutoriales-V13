bf278f1a97e32b830d46f3bcf2a475c86a83af5a
unknown_bom <73638811+AahhsSjsj@users.noreply.github.com>
2021-09-26 22:12:55 -0500

    Discord Bot V13
    Avatar Comando :)

--- Commands/info/avatar.js
+++ Commands/info/avatar.js
@@ -5,15 +5,17 @@ module.exports = {
     aliases: ['pfp'],
     description: 'Muestra El Avatar Del Usuario',
     cooldown: 5,
-    execute(message, args, commandName, client, Discord) {
+    run: async(message, args, commandName, client, Discord) => {
         const Target = message.mentions.users.first() || message.author;
 
         const responder = new MessageEmbed()
         .setColor('RANDOM')
         .setAuthor(`${Target.tag}\'s Avatar`)
         .setImage(Target.displayAvatarURL({ dynamic: true, size: 1024 }))
-        .setFooter(`Requerido Por ${message.author.tag}`, message.author.displayAvatarURL({ dynamic: true}))
+        .setFooter(
+           `Requerido Por ${message.author.tag}`, message.author.displayAvatarURL({ dynamic: true })
+        )
 
-        message.reply({embeds: [responder]})
+        message.reply({ embeds: [responder] })
     }
 }
