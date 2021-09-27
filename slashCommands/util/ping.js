54861bfa96beaa1945fe67aa3eae9c243b2c2b1c
unknown_bom <73638811+AahhsSjsj@users.noreply.github.com>
2021-09-26 22:08:56 -0500

    Update ping.js

--- slashCommands/util/ping.js
+++ slashCommands/util/ping.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'ping',
     description: 'mira el ping',
     run: async (client, interacion, args) => {
-        await interacion.followUp(`Mi Ping Es: {client.ws.ping}`)
+        await interacion.followUp(`Mi Ping Es: ${client.ws.ping}`)
     }
 }
